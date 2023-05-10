defa774fa2e9ac95194e53b65651e20db5ab4e0f
Code-jio <1054037036@qq.com>
2023-05-10 16:26:09 +0800

    删除多余webpack文件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+
+// react 有两种路由模式，一种是BrowserRouter、一种是HashRouter。
+// BrowserRouter会见厅URL的变化，当url变更事他将会使浏览器显示相应的页面
 import { BrowserRouter } from "react-router-dom"
 
 import "antd/dist/antd.less"
 import App from "./App"
 
+// 创建来自index.html的根标签
 const root = ReactDOM.createRoot(document.getElementById("App"));
-
+// 渲染根标签
 root.render(
     <BrowserRouter>
         <App />
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import "./index.less"
 
-export default function Home(params) {
+export default function Home(props) {
     return <div>
         <h1 className="home-title">Home~</h1>
     </div>
